f56876783d20eec81e65b18441ac63c475d26dbc
hithesh <hithescs123@gmail.com>
2024-11-25 12:25:04 +0530

    Add files via upload

---
+++ ruthu.py3.py
@@ -0,0 +1,32 @@
+numberl = 100
+print("The type of number1 ",type(number 1))
+
+integer _1 = 100
+integer _2 = 50
+print (integer_1*integer_2)
+print(integer_1+integer_2)
+print(integer_1-integer_2)
+print(integer_1/integer_2)  
+number2 = 100.0
+
+print("The type of number2 ",type(number 2))
+float_1 = 12.539
+float_2 = 6.78
+
+print (float_1*float_2)
+print(float_1+float_2)
+print(float_1-float_2)
+print (float_1/float_2)
+
+str = 'Jack Daniels'
+
+print("The type ofstr ",type(str)) string_1 = "Hello"
+string_2 = " World"
+print (string_1 + string_2)
+result = 100 < 200
+print("The type of result ",type(result))
+has_passed = False
+marks = 80
+if (marks > 50):
+  has_passed = True
+print (has_passed)
